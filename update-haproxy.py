b334649acb835fa20ccc204d615fa8af134968fc
Erik Bergsma <ebergsma1@gmail.com>
2014-06-04 13:05:46 +0200

    added functionality to restart by ubuntu service (now default) since the current default (standalone binary) was not working correctly, cleaned dead imports, moved the failover code to a new script, introduced an optional continous mode

--- update-haproxy.py
+++ update-haproxy.py
@@ -1,35 +1,44 @@
-import boto.ec2.connection
-from boto.ec2.connection import EC2Connection
-from boto.ec2.securitygroup import SecurityGroup
-from boto.ec2.instance import Instance
-from mako.template import Template
-import argparse
-import os
+import argparse 
 import subprocess
 import logging
-from haproxy_autoscale import get_running_instances, file_contents, generate_haproxy_config, steal_elastic_ip
-import urllib2
+import sys
+import time
+from haproxy_autoscale import get_running_instances, file_contents, generate_haproxy_config, restart_haproxy
 
-
-def main():
+def parse_args():
     # Parse up the command line arguments.
     parser = argparse.ArgumentParser(description='Update haproxy to use all instances running in a security group.')
     parser.add_argument('--security-group', required=True, nargs='+', type=str)
     parser.add_argument('--access-key', required=True)
     parser.add_argument('--secret-key', required=True)
     parser.add_argument('--output', default='haproxy.cfg',
-                        help='Defaults to haproxy.cfg if not specified.')
-    parser.add_argument('--template', default='templates/haproxy.tpl')
-    parser.add_argument('--haproxy', default='./haproxy',
-                        help='The haproxy binary to call. Defaults to haproxy if not specified.')
+                        help='Defaults to ./haproxy.cfg if not specified.')
+    parser.add_argument('--template', default='templates/haproxy.tpl',
+                        help="the template you want to use for generating the HAproxys config file")
+    parser.add_argument('--servicename', default="haproxy", 
+                        help='The OS service name to restart (Ubuntu only)')
+    parser.add_argument('--haproxy', default=None,
+                        help='The haproxy binary to call. Defaults to haproxy if not specified.\n\
+                        Not needed if --servicename is used')
     parser.add_argument('--pid', default='/var/run/haproxy.pid',
-                        help='The pid file for haproxy. Defaults to /var/run/haproxy.pid.')
-    parser.add_argument('--eip',
-                        help='The Elastic IP to bind to when VIP seems unhealthy.')
-    parser.add_argument('--health-check-url',
-                        help='The URL to check. Assigns EIP to self if health check fails.')
+                        help='The pid file for haproxy. Defaults to /var/run/haproxy.pid.\n\
+                        Not needed if --servicename is used')
+    parser.add_argument('--sleep', default=False, type=int,
+                        help='If specified this script will go in a continous loop, sleeping this amount between runs.')
+    
     args = parser.parse_args()
-
+    
+    # syntax checking
+    if args.servicename != "haproxy" and args.haproxy:
+        logging.fatal("you must supply either \"--servicename\" OR \"--haproxy\", dont know what to restart now")
+        sys.exit(2)
+    
+    if args.servicename == "haproxy" and not args.haproxy:
+        logging.info("no \"--servicename\" OR \"--haproxy\" arguments found, defaulting to restarting service haproxy")
+    
+    return args
+    
+def main(args):    
     # Fetch a list of all the instances in these security groups.
     instances = {}
     for security_group in args.security_group:
@@ -37,6 +46,7 @@ def main():
         instances[security_group] = get_running_instances(access_key=args.access_key,
                                                           secret_key=args.secret_key,
                                                           security_group=security_group)
+
     # Generate the new config from the template.
     logging.info('Generating configuration for haproxy.')
     new_configuration = generate_haproxy_config(template=args.template,
@@ -46,6 +56,7 @@ def main():
     # Otherwise just delete the temporary file and do nothing.
     logging.info('Comparing to existing configuration.')
     old_configuration = file_contents(filename=args.output)
+    
     if new_configuration != old_configuration:
         logging.info('Existing configuration is outdated.')
         
@@ -55,37 +66,20 @@ def main():
                       content=generate_haproxy_config(template=args.template,
                                                       instances=instances    ))
         
-        # Get PID if haproxy is already running.
-        logging.info('Fetching PID from %s.' % args.pid)
-        pid = file_contents(filename=args.pid)
+        restart_haproxy(args)
         
-        # Restart haproxy.
-        logging.info('Restarting haproxy.')
-        command = '''%s -p %s -f %s -sf %s''' % (args.haproxy, args.pid, args.output, pid or '')
-        logging.info('Executing: %s' % command)
-        subprocess.call(command, shell=True)
     else:
         logging.info('Configuration unchanged. Skipping restart.')
     
-    # Do a health check on the url if specified.
-    try:
-        if args.health_check_url and args.eip:
-            logging.info('Performing health check.')
-            try:
-                logging.info('Checking %s' % args.health_check_url)
-                response = urllib2.urlopen(args.health_check_url)
-                logging.info('Response: %s' % response.read())
-            except:
-                # Assign the EIP to self.
-                logging.warn('Health check failed. Assigning %s to self.' % args.eip)
-                steal_elastic_ip(access_key=args.access_key,
-                                 secret_key=args.secret_key,
-                                 ip=args.eip                )
-    except:
-        pass
-
-
-
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    main()
+    args = parse_args()
+    
+    if args.sleep: #continous runs
+        logging.info("continous mode, sleeping %i seconds between runs\n" % args.sleep)
+        while True:
+            main(args)
+            time.sleep(args.sleep)
+    
+    else: #standard, onetime run
+        main(args)
