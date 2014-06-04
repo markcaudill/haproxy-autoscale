a0d86b8023a0dffef73e3a0b7a35eb4e9c58f2f4
Erik Bergsma <ebergsma1@gmail.com>
2014-06-04 13:05:20 +0200

    higher version of boto because of the need for the s.instances() function

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup
 
 setup(name='haproxy-autoscale',
-      version='0.4',
+      version='0.4.1',
       description='HAProxy wrapper for handling auto-scaling EC2 instances.',
       author='Mark Caudill',
       author_email='mark@markcaudill.me',
       url='https://github.com/markcaudill/haproxy-update',
-      install_requires=['boto>=2.0', 'mako>=0.5.0', 'argparse>=1.2.1'],
-      license='MIT'                                                          )
+      install_requires=['boto>=2.9', 'mako>=0.5.0', 'argparse>=1.2.1'],
+      license='MIT'
+      )
